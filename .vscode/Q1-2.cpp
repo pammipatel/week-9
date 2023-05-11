793cb52b9aeebfa4c519b34acba672d0a9c7d1af
pammi patel <a1838486@adelaide.edu.au>
2023-05-11 11:30:09 +0930

    Q1-2 done properly

--- .vscode/Q1-2.cpp
+++ .vscode/Q1-2.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 
 int** arrayOfArrays(int n_rows, int n_cols) {
-    int** arr = new int*[n_rows];  // Create an array of pointers
+    int** arr = new int*[n_rows];  
 
     for (int i = 0; i < n_rows; i++) {
-        arr[i] = new int[n_cols];  // Create each row as an array of integers
+        arr[i] = new int[n_cols];  
 
         for (int j = 0; j < n_cols; j++) {
             arr[i][j] = 1;  
         }
     }
-
-    return arr;  // Return pointer to the first element 
+  
+    return arr;  
+}
